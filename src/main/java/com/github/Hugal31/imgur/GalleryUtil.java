577998f650100f008c7dec33997f84b7f5a7428e
Hugo Laloge <hugo.laloge@gmail.com>
2017-02-19 17:12:18 +0100

    fix: Fix illegal diamond operator in Java 1.5

--- src/main/java/com/github/Hugal31/imgur/GalleryUtil.java
+++ src/main/java/com/github/Hugal31/imgur/GalleryUtil.java
@@ -9,7 +9,7 @@ import java.util.List;
 final class GalleryUtil {
 
     static List<ImgurItem> createGallery(JSONArray data) {
-        List<ImgurItem> itemList = new ArrayList<>();
+        List<ImgurItem> itemList = new ArrayList<ImgurItem>();
         for (Object object: data) {
             if (((JSONObject) object).has("cover"))
                 itemList.add(AlbumUtil.createAlbum((JSONObject) object));
